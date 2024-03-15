87e8965905a9105effe22a28987ad607500b3362
Leonel Cordero <leonelrcordero22@gmail.com>
2024-03-14 23:19:23 -0300

    Agregue los middlewares necesarios para el proyecto

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import morgan from 'morgan';
+
+const app = express();
+app.set('port', process.env.PORT || 4024);
+app.listen(app.get('port'), () => {
+  console.log('Estoy en el puerto ' + app.get('port'));
+});
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 console.log('Ejecutado desde index.js');
