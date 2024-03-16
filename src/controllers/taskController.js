650c0304afebb768f0a061fc309abcb82bef9123
Leonel <leonelrcordero22@gmail.com>
2024-03-15 21:20:27 -0300

    Se agrego logica en caso de que la base de datos falle

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -6,32 +6,46 @@ export const getTask = async (req, res) => {
 };
 
 export const setTask = async (req, res) => {
-  const { name } = req.body;
-  const taskCreate = new taskModel({
-    name: name,
-  });
-  taskCreate.save();
-  res.status(200).json({
-    message: 'Objeto cargado',
-    object: {
+  try {
+    const { name } = req.body;
+    const taskCreate = new taskModel({
       name: name,
-    },
-  });
+    });
+    taskCreate.save();
+    res.status(200).json({
+      message: 'Objeto cargado',
+      object: {
+        name: name,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error interno',
+      error: error,
+    });
+  }
 };
 
 export const taskDelete = async (req, res) => {
-  const { id } = req.params;
-  const task = await taskModel.findByIdAndDelete({ _id: id });
-  console.log(task);
-  if (task) {
-    res.status(200).json({
-      message: 'Tarea eliminada.',
-      object: task,
-    });
-  } else {
-    res.status(404).json({
-      message: 'Tarea no encontrada.',
-      object: task,
+  try {
+    const { id } = req.params;
+    const task = await taskModel.findByIdAndDelete({ _id: id });
+    console.log(task);
+    if (task) {
+      res.status(200).json({
+        message: 'Tarea eliminada.',
+        object: task,
+      });
+    } else {
+      res.status(404).json({
+        message: 'Tarea no encontrada.',
+        object: task,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error interno',
+      error: error,
     });
   }
 };
